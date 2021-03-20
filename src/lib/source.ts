60b8e2a9744d7dc06448939958d6dc2df4830d42
edu <edu@emai.com>
2021-03-20 17:05:20 -0500

    feat: add source factory

---
+++ src/lib/source.ts
@@ -0,0 +1,24 @@
+import {readFileSync} from 'fs';
+import {parse as Json5Parse} from 'json5';
+import {parse as YamlParse} from 'yaml';
+import {parse as TomlParse} from 'toml';
+
+export abstract class SourceFactory{
+
+    static read(path : string, format : string){
+        const source = readFileSync(path, {
+            encoding: 'utf-8'
+        });
+        switch(format){
+            case 'json':
+                return JSON.parse(source);
+            case 'json5':
+                return Json5Parse(source);
+            case 'yaml':
+                return YamlParse(source);
+            case 'toml':
+                return TomlParse(source);
+        }
+    }
+
+}
