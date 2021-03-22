38e506a74bdb4336d2f3c23d86c7476cd3b89985
edu <edu@emai.com>
2021-03-21 23:00:31 -0500

    feat: add prompt

--- src/lib/analyzer.ts
+++ src/lib/analyzer.ts
@@ -1,18 +1,19 @@
 import {isAbsolute, resolve} from 'path';
 import {SourceFactory} from './source';
-import {has as ObjectHas, get as ObjectGet} from 'dot-prop';
+import * as Inquirer from 'inquirer';
+import {has as ObjectHas} from 'dot-prop';
 
 export abstract class Analyzer{
 
-    static process(
+    static async process(
         root : RootDefinition | RootDefinition[],
-        cwd : string
+        options : OptionsAnalyzer
     ){
         const rootAnalyzer = new RootAnalyzer(
             root,
-            cwd
+            options
         );
-        rootAnalyzer.analyze();
+        await rootAnalyzer.analyze();
     }
 
 }
@@ -21,61 +22,88 @@ class RootAnalyzer{
 
     constructor(
         private _root : RootDefinition | RootDefinition[],
-        private _cwd : string,
+        private _options : OptionsAnalyzer,
     ){}
 
-    analyze(){
+    async analyze(){
         const root = this._root instanceof Array ? this._root : [this._root];
-        root.forEach((element) => {
+        const errors = [];
+        for(const key in root){
+            const element = root[key];
             const filePath = isAbsolute(element.file) 
                 ? element.file 
-                : resolve(this._cwd, element.file);
+                : resolve(this._options.cwd, element.file);
             const dataSource = SourceFactory.read(
                 filePath,
                 element?.format ?? 'json'
             );
-            this.search(
+            await this.search(
                 element.content,
                 dataSource,
                 filePath,
+                errors,
                 ''
             );
-        });
+        }
+        if(errors.length > 0){
+            errors.forEach((element) => {
+                console.error(element);
+            });
+            process.exit(1);
+        }
     }
 
-    private search(
+    private async search(
         contentRoot: any,
         contentSource : any,
         file : string,
+        errors : string[],
         location : string
     ){
         if(!contentRoot) return;
-        const keys = Object.keys(contentRoot);
-        keys.forEach((element) => {
+        for(const element in contentRoot){
             const ref = contentRoot[element];
             let actualLocation = location + 
                                 (!(location === '') ? '.' : '') +
                                 element;
             if(ref.required === true){
                 if(!ObjectHas(contentSource, actualLocation)){
-                    console.error(`In file: ${file} -> '${actualLocation}' pattern dont exists`);
-                    process.exit(1);
+                    const message = `In file: ${file} -> '${actualLocation}' pattern dont exists`;
+                    if(this._options.stop){
+                        console.error(message);
+                        process.exit(1);
+                    }else{
+                        errors.push(message);
+                    }
                 }
             }
             if(ref.warning === true){
                 if(!ObjectHas(contentSource, actualLocation)){
-                    // TODO
+                    const answers = await Inquirer
+                        .prompt([
+                            {
+                                type: 'confirm',
+                                name: 'question',
+                                default: true,
+                                message: `In file: ${file} -> '${actualLocation}' pattern dont exists, continue?`
+                            },
+                        ]);
+                    if(!answers.question){
+                        console.error(`In file: ${file} -> '${actualLocation}' pattern dont exists`);
+                        process.exit(1);
+                    }
                 }
             }
             if(ObjectHas(ref, 'content')){
-                this.search(
+                await this.search(
                     ref.content,
                     contentSource,
                     file,
+                    errors,
                     actualLocation
                 );
             }
-        });
+        }
     }
 
 }
@@ -84,4 +112,9 @@ export type RootDefinition = {
     file : string,
     format: 'json' | 'json5' | 'yaml' | 'toml' | 'elcd',
     content : any
+}
+
+export type OptionsAnalyzer = {
+    cwd : string,
+    stop : boolean
 }
