a1c15c6ee3d61657bc61cb1956801764b5edd553
edu <edu@emai.com>
2021-03-24 23:47:36 -0500

    fix: modify cli

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -7,12 +7,11 @@ import { Analyzer } from './analyzer';
 export function cli(){
     const program = new Command();
     program.version('1.0.0', '-v, --version', 'current version');
-    program.arguments('<event>')
-    program.option('-f, --format', 'format of file', 'json');
-    program.option('-c, --config', 'path of file', process.cwd());
-    program.option('-n, --name-file', 'name file', '.preactionrc');
+    program.arguments('[event]');
+    program.option('-f, --format <format>', 'format of file', 'json');
+    program.option('-c, --config <path>', 'path of file', process.cwd());
+    program.option('-n, --name-file <name>', 'name file', '.preactionrc');
     program.option('-s, --stop', 'stop process when condition failed', true);
-    program.parse(process.argv);
     program.action((event, options) => {
         const mainConfigRoot = SourceFactory.read(
             resolve(options.config, options.nameFile),
@@ -23,4 +22,5 @@ export function cli(){
             options
         );
     });
+    program.parse(process.argv);
 }
